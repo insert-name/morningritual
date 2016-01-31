3166a901d8fe2d700f07e2824fe4f0f54cfd133a
TenginE123 <matt_thomas90@hotmail.co.uk>
2016-01-31 01:10:03 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	planning/level-format.txt

--- src/cellnew.h
+++ src/cellnew.h
@@ -9,63 +9,63 @@ namespace MorningRitual
 		CGT_FURNITURE,
 		CGT_UPSTAIR,
 		CGT_DOWNSTAIR,
-	};
+	}:
 	
 	enum CellType
 	{
-		CT_EMPTY,
-		CT_POND,
+		CT_EMPTY	= ' ',
+		CT_POND		= 'P',
 
 		CT_WOOD_FLOOR		= '.',
 		CT_KITCHEN_FLOOR	= ',',
-		CT_BATHROOM_FLOOR	= ';',
+		CT_BATHROOM_FLOOR	= ':',
 		CT_CARPET_FLOOR		= ':',
 		CT_GRASS			= '*',
-		CT_PAVING_SLAB		= '`',
+		CT_PAVING_SLAB		= '"',
 
-		CT_WALL_H, = 'H'
-		CT_WALL_V, = 'V'
-		CT_WALL_C, = 'C'
-		CT_WALL_T, = 'T'
-		CT_WALL_X, = 'X'
-		CT_WINDOW_SMALL, = '|'
-		CT_WINDOW_LARGE_L, = '\'
-		CT_WINDOW_LARGE_R, = '/'
+		CT_WALL_H,	= 'H'
+		CT_WALL_V,	= 'V'
+		CT_WALL_C,	= 'C'
+		CT_WALL_T,	= 'T'
+		CT_WALL_X,	= 'X'
+		CT_WINDOW_SMALL,	= '|'
+		CT_WINDOW_LARGE_L,	= '\'
+		CT_WINDOW_LARGE_R,	= '/'
 		
-		CT_DOOR_OPEN, = ''
-		CT_DOOR_CLOSED, = ''
+		CT_DOOR_OPEN,	= '+'
+		CT_DOOR_CLOSED,	= '-'
 		
 		//Furniture
-		CT_TABLE,
-		CT_CHAIR,
+		CT_TABLE	= 't',
+		CT_CHAIR	= 'c',
 
-		CT_K_COUNTER_STRAIGHT,
-		CT_K_COUNTER_INNER,
-		CT_K_COUNTER_OUTER,
-		CT_K_COUNTER_DOOR,
-		CT_K_SINK,
-		CT_K_OVEN,
-		CT_K_OVEN_DOOR,
-		CT_K_FRIDGE,
-		CT_K_FRIDGE_DOOR,
-		CT_K_WCHINE,
+		CT_K_COUNTER_STRAIGHT	= '1',	
+		CT_K_COUNTER_INNER		= '2',		
+		CT_K_COUNTER_OUTER		= '3',		
+		CT_K_COUNTER_DOOR		= '4',		
+		CT_K_OVEN			= 'O',
+		CT_K_OVEN_DOOR		= 'o',
+		CT_K_FRIDGE			= 'F',
+		CT_K_FRIDGE_DOOR	= 'f',
+		CT_K_WCHINE			= 'W',
+		CT_K_SINK			= 'S',
 
-		CT_L_SHOWER,
-		CT_L_TOILET,
-		CT_L_SINK,
-		CT_L_BATH_UPPER,
-		CT_L_BATH_LOWER,
+		CT_L_SHOWER		= 'p',
+		CT_L_TOILET		= 'T',
+		CT_L_SINK		= 's',
+		CT_L_BATH_UPPER = 'B',
+		CT_L_BATH_LOWER = 'b',
 
-		CT_B_BED_UPPER,
-		CT_B_BED_LOWER,
-		CT_B_TABLE,
-		CT_B_WARDROBE_L,
-		CT_B_WARDROBE_R,
-		CT_B_WARDROBE_L_DOOR,
-		CT_B_WARDROBE_R_DOOR,
+		CT_B_BED_UPPER	= '(',
+		CT_B_BED_LOWER	= ')',
+		CT_B_TABLE		= 'Y',
+		CT_B_WARDROBE_L = '[',
+		CT_B_WARDROBE_R = ']',
+		CT_B_WARDROBE_L_DOOR = '9',
+		CT_B_WARDROBE_R_DOOR = '0',
 
-		CT_UPSTAIR,
-		CT_DOWNSTAIR,
+		CT_UPSTAIR		= '<',
+		CT_DOWNSTAIR	= '>',
 	}
 	
 	struct Cell
@@ -78,22 +78,63 @@ namespace MorningRitual
 			{
 				switch(this->id)
 				{
-					case CellType::EMPTY:
-					case CellType::POND:
-					return CellGenericType::EMPTY;
+					case CellType::CT_EMPTY:
+					case CellType::CT_POND:
+					return CellGenericType::CGT_EMPTY;
 						break;
 					
-					case CellType::WOOD_FLOOR:
-					case CellType::KITCHEN_FLOOR:
-					case CellType::BATHROOM_FLOOR;
-					return CellGenericType::FLOOR;
+					case CellType::CT_WOOD_FLOOR:
+					case CellType::CT_KITCHEN_FLOOR:
+					case CellType::CT_BATHROOM_FLOOR:
+					case CellType::CT_CARPET_FLOOR:
+					case CellType::CT_GRASS:
+					case CellType::CT_PAVING_SLAB:
+					return CellGenericType::CGT_FLOOR;
 						break;
 					
-					case CellType::WALL_H;
-					return CellGenericType::WALL;
+					case CellType::CT_WALL_H:
+					case CellType::CT_WALL_V:
+					case CellType::CT_WALL_C:
+					case CellType::CT_WALL_T:
+					case CellType::CT_WALL_X:
+					case CellType::CT_WINDOW_SMALL:
+					case CellType::CT_WINDOW_LARGE_L:
+					case CellType::CT_WINDOW_LARGE_R:
+					return CellGenericType::CGT_WALL;
 						break;
 
+					case CellType::CT_DOOR_OPEN:
+					case CellType::CT_DOOR_CLOSED:
+					return CellGenericType::CGT_DOOR;
+						break;
+
+					case CellType::CT_K_COUNTER_STRAIGHT:
+					case CellType::CT_K_COUNTER_INNER:
+					case CellType::CT_K_COUNTER_OUTER:
+					case CellType::CT_K_COUNTER_DOOR:
+					case CellType::CT_K_OVEN:
+					case CellType::CT_K_OVEN_DOOR:
+					case CellType::CT_K_FRIDGE:
+					case CellType::CT_K_FRIDGE_DOOR:
+					case CellType::CT_K_WCHINE:
+					case CellType::CT_K_SINK:
+														
+					case CellType::CT_L_SHOWER:
+					case CellType::CT_L_TOILET:
+					case CellType::CT_L_SINK:
+					case CellType::CT_L_BATH_UPPER:
+					case CellType::CT_L_BATH_LOWER:
+
+					case CellType::CT_B_BED_UPPER:
+					case CellType::CT_B_BED_LOWER:
+					case CT_B_TABLE:
+					case CT_B_WARDROBE_L:
+					case CT_B_WARDROBE_R:
+					case CT_B_WARDROBE_L_DOOR:
+					case CT_B_WARDROBE_R_DOOR:
+					return CellGenericType::CGT_FURNITURE;
+						break;
 				}
 			}
-	};
+	}:
 }
