347e7bafb564775d2c5e28d00ee640b4cd1f9350
Joshua Barretto <barry.of.smith@gmail.com>
2016-01-31 08:25:33 +0000

    Greatly enhanced title menus

--- src/soundmanager.cpp
+++ src/soundmanager.cpp
@@ -22,10 +22,27 @@ namespace MorningRitual
 		this->musics[MusicType::PANIC_2].openFromFile(this->data_directory + "/audio/music/MUSIC_gameplay_danger_high_010.ogg");
 	}
 	
+	void SoundManager::update()
+	{
+		if (this->back_muted)
+		{
+			for (int i = 0; i < 10; i ++)
+				this->musics[i].setVolume(0);
+		}
+		else
+		{
+			for (int i = 0; i < 10; i ++)
+				this->musics[i].setVolume(100);
+		}
+	}
+	
 	void SoundManager::playSound(SoundType type)
 	{
-		this->sounds[type].setBuffer(this->sound_buffers[type]);
-		this->sounds[type].play();
+		if (!this->sound_muted)
+		{
+			this->sounds[type].setBuffer(this->sound_buffers[type]);
+			this->sounds[type].play();
+		}
 		
 		printf("Played sound\n");
 	}
