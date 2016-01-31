09c349ae125ed2a3b44421780e2dd4f607f6333f
TenginE123 <matt_thomas90@hotmail.co.uk>
2016-01-31 02:00:22 +0000

    Tiles and Solidity

--- src/cellnew.h
+++ src/cellnew.h
@@ -128,6 +128,66 @@ namespace MorningRitual
 				}
 			}
 			
+			bool getSolid() 
+			{
+				switch (this->id)
+				{
+					case CellType::CT_EMPTY:
+					case CellType::CT_POND:
+
+					case CellType::CT_WALL_H:
+					case CellType::CT_WALL_V:
+					case CellType::CT_WALL_C:
+					case CellType::CT_WALL_T:
+					case CellType::CT_WALL_X:
+					case CellType::CT_WINDOW_SMALL:
+					case CellType::CT_WINDOW_LARGE_L:
+					case CellType::CT_WINDOW_LARGE_R:
+
+					case CellType::CT_DOOR_CLOSED:
+
+					case CellType::CT_K_COUNTER_STRAIGHT:
+					case CellType::CT_K_COUNTER_INNER:
+					case CellType::CT_K_COUNTER_OUTER:
+					case CellType::CT_K_OVEN:
+					case CellType::CT_K_FRIDGE:
+					case CellType::CT_K_WCHINE:
+					case CellType::CT_K_SINK:
+
+					case CellType::CT_B_WARDROBE_L:
+					case CellType::CT_B_WARDROBE_R:
+					return true;
+						break;
+
+					case CellType::CT_WOOD_FLOOR:
+					case CellType::CT_KITCHEN_FLOOR:
+					case CellType::CT_BATHROOM_FLOOR:
+					case CellType::CT_CARPET_FLOOR:
+					case CellType::CT_GRASS:
+					case CellType::CT_PAVING_SLAB:
+
+					case CellType::CT_DOOR_OPEN:
+
+					case CellType::CT_K_COUNTER_DOOR:
+					case CellType::CT_K_OVEN_DOOR:
+					case CellType::CT_K_FRIDGE_DOOR:
+
+					case CellType::CT_L_SHOWER:
+					case CellType::CT_L_TOILET:
+					case CellType::CT_L_SINK:
+					case CellType::CT_L_BATH_UPPER:
+					case CellType::CT_L_BATH_LOWER:
+
+					case CellType::CT_B_BED_UPPER:
+					case CellType::CT_B_BED_LOWER:
+
+					case CellType::CT_B_WARDROBE_L_DOOR:
+					case CellType::CT_B_WARDROBE_R_DOOR:
+						return false;
+						break;
+				}
+			}
+
 			CellGenericType getGenericType()
 			{
 				switch(this->id)
