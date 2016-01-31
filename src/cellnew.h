ec6bdf0075027df316785ef38f05cd4a6002a6ee
TenginE123 <matt_thomas90@hotmail.co.uk>
2016-01-31 01:45:57 +0000

    TexCoords

--- src/cellnew.h
+++ src/cellnew.h
@@ -18,7 +18,7 @@ namespace MorningRitual
 
 		CT_WOOD_FLOOR		= '.',
 		CT_KITCHEN_FLOOR	= ',',
-		CT_BATHROOM_FLOOR	= ':',
+		CT_BATHROOM_FLOOR	= ';',
 		CT_CARPET_FLOOR		= ':',
 		CT_GRASS			= '*',
 		CT_PAVING_SLAB		= '"',
@@ -51,7 +51,7 @@ namespace MorningRitual
 		CT_K_SINK			= 'S',
 
 		CT_L_SHOWER		= 'p',
-		CT_L_TOILET		= 'T',
+		CT_L_TOILET		= 'u',
 		CT_L_SINK		= 's',
 		CT_L_BATH_UPPER = 'B',
 		CT_L_BATH_LOWER = 'b',
@@ -73,6 +73,60 @@ namespace MorningRitual
 		public:
 			int id;
 			int data;
+
+			sf::Vector2u getTexCoords()
+			{
+				switch (this->id)
+				{
+				case CellType::CT_EMPTY:
+					return sf::Vector2u(0, 0);
+					break;
+
+				case CellType::CT_WOOD_FLOOR:
+					return sf::Vector2u(1, 0);
+					break;
+
+				case CellType::CT_KITCHEN_FLOOR:
+					return sf::Vector2u(2, 0);
+					break;
+
+				case CellType::CT_BATHROOM_FLOOR:
+					return sf::Vector2u(1, 1);
+					break;
+
+				case CellType::CT_CARPET_FLOOR:
+					return sf::Vector2u(2, 1);
+					break;
+
+				case CellType::CT_WALL_H:
+					return sf::Vector2u(3, 0);
+					break;
+
+				case CellType::CT_WALL_V:
+					return sf::Vector2u(3, 1);
+					break;
+
+				case CellType::CT_WALL_C:
+					return sf::Vector2u(4, 0);
+					break;
+
+				case CellType::CT_WALL_T:
+					return sf::Vector2u(6, 0);
+					break;
+
+				case CellType::CT_WALL_X:
+					return sf::Vector2u(8, 0);
+					break;
+
+				case CellType::CT_WALL_V:
+					return sf::Vector2u(3, 1);
+					break;
+
+				default:
+					return sf::Vector2u(0, 0);
+					break;
+				}
+			}
 			
 			CellGenericType getGenericType()
 			{
@@ -127,11 +181,11 @@ namespace MorningRitual
 
 					case CellType::CT_B_BED_UPPER:
 					case CellType::CT_B_BED_LOWER:
-					case CT_B_TABLE:
-					case CT_B_WARDROBE_L:
-					case CT_B_WARDROBE_R:
-					case CT_B_WARDROBE_L_DOOR:
-					case CT_B_WARDROBE_R_DOOR:
+					case CellType::CT_B_TABLE:
+					case CellType::CT_B_WARDROBE_L:
+					case CellType::CT_B_WARDROBE_R:
+					case CellType::CT_B_WARDROBE_L_DOOR:
+					case CellType::CT_B_WARDROBE_R_DOOR:
 					return CellGenericType::CGT_FURNITURE;
 						break;
 				}
